8dd95fb6e362e677706e52b20cced5015492bec9
Patrick Celedonio <patrickceledonio@gmail.com>
2013-09-10 21:18:03 +0800

    module 8

--- module-eight/js/app.js
+++ module-eight/js/app.js
@@ -1,25 +1,35 @@
 $(function() {
-	
-	var app = {};
-	var url = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json';
-	$.ajax({
-			url: url,
-			data: {
-				apiKey: 'kzydjpv969n5v7hzumzeyb5c'
-			},
-			dataType: 'jsonp',
-			success: showBoxOffice
-	});
 
-	function showBoxOffice(response){
-		var movie;
-		for(var i = 0; i < response.movies.length; i++){
-			movie = response.movies[i];
-			var template = $('#tpl-movie').html();
-			var $template = Handlebars.compile(template);
-			var markup = $template(movie);
-			console.log('markup', markup);
-			$('ul').append(markup);
-		}
-	}
+    var app = {};
+    var url = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json';
+    $.ajax({
+        url: url,
+        data: {
+            apiKey: 'kzydjpv969n5v7hzumzeyb5c'
+        },
+        dataType: 'jsonp',
+        success: showBoxOffice
+    });
+    function getTemplate(template_id, context) {
+        var template, $template, markup;
+        template = $('#' + template_id);
+        $template = Handlebars.compile(template.html());
+        markup = $template(context);
+        return markup;
+
+    }
+    function showBoxOffice(response) {
+        app.movies = response.movies;
+        var movie, template, $template, markup;
+        for (var i = 0; i < app.movies.length; i++) {
+                movie = app.movies[i];
+                movie._index = i;
+                $('table tbody').append(getTemplate('tpl-box-office-item', movie));
+        }
+        $('table tbody>tr').hover(function(ev) {
+            var data = $(ev.target).closest('tr').data();
+            var movie = app.movies[data.id];
+            $('.movie-detail').html(getTemplate('tpl-movie-detail', movie));
+        });
+    }
 });
